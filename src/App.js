3257d52cdb32c4696eac4c180f2c2bcd38bfa537
Justice A. Acheampong <just.appiah@gmail.com>
2022-03-28 00:48:21 -0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import groceStuff from "./groceStuff"
 
 function App() {
    //keeps track of list and update it
-  const [list, setList] = useState([])
+  const [list, setList] = useState(Grocery)
 
-
+  const addGroce = () => {
+    setList(list.concat([{
+      item: document.querySelector("item").value,
+      brand: document.querySelector("brand").value,
+      unit: document.querySelector("unit").value,
+      quantity: document.querySelector("quantity").value,
+      isPurchased: false
+    }]))
+  }
 
  useEffect(() => {
-   document.querySelector("#button").addEventListener('click')
+   document.querySelector("#button").addEventListener('click', addGroce)
 
   })
 
@@ -48,98 +56,3 @@ function App() {
 
 export default App;
 
-
-  // const handleAddItem = () => {
-  //   const newList = [...list, {title: inputData}]
-  //    //receives new list of items
-  //     setlist(newList);
-  //     setInputData('')
-  //     console.log(list)
-  // }
-
-  //keeps track of input field
-  // const [inputData, setInputData] = useState('')
-
-  // useEffect(() => {
-  //   const handleRemoveItem = (index) => {
-  //     const newList = [];
-  //     for (let i =0; i < list.length; i++) {
-  //       if (index !== i) {
-  //         newList.push(list[i])
-  //       }
-  //     }
-  //     setlist(newList)
-  //   }
-  // })
-  
-
-
-  // return (
-  //   <div className="App">
-  //     <h1>Grocery Store</h1>
-  //     <div className="input">
-        //   {/* handles the change in the value of an input element */}
-        // {/* <input type="text" value={inputData} onChange={(event) => setInputData(event.taret.value)}></input> */}
-        // {/* <input type="text" name="item" placeholder="Item"></input>
-        // <input type="text" name="brand" placeholder="Brand"></input>
-        // <input type="text" name="unit" placeholder="Units"></input>
-        // <input type="text" name="quantity" placeholder="Quantity"></input> */}
-
-        // {/* <input type="submit" value="ADD" onClick={() => handleAddItem()}></input> */}
-       
-      // </div>
-     
-
-  //   </div>
-  // )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// return (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//         className="App-link"
-//         href="https://reactjs.org"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Groceries hoMEWORK
-//       </a>
-//     </header>
-//   </div>
-// );
--- src/Grocery.js
+++ src/Grocery.js
@@ -23,19 +23,7 @@
        }
 
 ]
-        //     return (
-        // <div className="stuff">
-        //     <h2> item:{props.groceStuff.item} </h2>
-        //     <h2> brand:{props.groceStuff.brand} </h2>
-        //     <h2> units:{props.groceStuff.units} </h2>
-        //     <h2> quantity:{props.groceStuff.quantity} </h2>
-        //     <button onClick={this.handleRemove}>Remove</button>
-        //       <p> 
-        //       isPurchased:{groceStuff.isPurchased ? "true" : "false"} 
-        //       </p>
-
-        // </div>
-        // )
+        
  export default grocery;
 
 
@@ -45,29 +33,3 @@
 
 
 
-//   return groceStuff.keys(groceStuff).map(stuff => {
-    
-
-//     return (
-//         <div className="stuff">
-//             <h2> item:{props.groceStuff.item} </h2>
-//             <h2> brand:{props.groceStuff.brand} </h2>
-//             <h2> units:{props.groceStuff.units} </h2>
-//             <h2> quantity:{props.groceStuff.quantity} </h2>
-//             <button onClick={this.handleRemove}>Remove</button>
-//               <p> 
-//               isPurchased:{groceStuff.isPurchased ? "true" : "false"} 
-//               </p>
-
-//         </div>
-//         )
-//     })
-    
-       
-//                     // { groceStuff.isPurchased > 0 ? 
-//           // <input id="remove" type="submit" value="REMOVE"/> : null }
-    
-
-//   }
-
-//   export default Grocery;
